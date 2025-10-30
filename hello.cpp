3c55b03dabba9bf423c5d5251cfbc1613a49078c
Miles😏 <162285816+MilesOx@users.noreply.github.com>
2025-10-30 07:38:08 +0000

    initial commit

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+// include library for input and output
+
+int main() {
+    // main function - execution starts here
+    std::cout << "Hello, world!\n";
+    // print Hello, world! to the terminal
+    return 0;
+    // return 0 to indicate successful completion
+}
